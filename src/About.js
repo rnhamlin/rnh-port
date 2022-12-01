39728b8be1489361eea78aa59ec22af8d86b156c
reneenh <reneehamlinteaches@gmail.com>
2022-11-30 22:54:02 -0800

    fixing merge error--file was deleted somehow? google what happened there.

---
+++ src/About.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function About() {
+  return (
+    <section>
+      <h1 id="about">Renée Hamlin</h1>
+      <br />
+      <p>
+        With a background in education and writing, Renée has long had an
+        interest in developing interesting, dynamic web applications. She looks
+        forward to developing projects that align with her interests (such as
+        volcanos, poetry, and organization). She has had several poems published
+        in various literary journals, and hopes to see her web applications live
+        online, as well!
+      </p>
+    </section>
+  );
+}
+
+export default About;
