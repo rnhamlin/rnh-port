391b9fb339340984e6530067a63503586997cfcc
reneenh <reneehamlinteaches@gmail.com>
2022-12-05 12:40:02 -0800

    added error message functionality to contact component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,24 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Contact() {
+  const [errorMessage, setErrorMessage] = useState("");
+  function handleChange(event) {
+    console.dir(event.target.value);
+    // if event (!value="")
+    if (event.target.value === "") {
+      console.log("I need to throw an error");
+      setErrorMessage("error-message");
+    } else {
+      setErrorMessage("");
+    }
+  }
   return (
-    <section>
+    <section className="formparent">
       <h2>Contact</h2>
       <br />
       <p>I would love to hear from you!</p>
-      <form class="contactform">
-        <label>
-          Name: 
-          <input type="text" />
-        </label>
-        <label>
-          Email Address: 
-          <input type="text"/>
-        </label>
-        <label>
-          Message: 
-          <input type="text"/>
-        </label>
+      {errorMessage !== "" ? <h3>{errorMessage}</h3> : <></>}
+      <form className="contactform">
+        <label>Name:</label>
+        <input onBlur={handleChange} className="form-input" type="text" />
+        <label>Email Address:</label>
+        <input onBlur={handleChange} className="form-input" type="text" />
+        <label>Message:</label>
+        <textarea onBlur={handleChange} className="form-input" type="text" />
+        <button>Send email!</button>
       </form>
     </section>
   );
