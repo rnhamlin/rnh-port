e348e22b385ba33ed0e24651a80be6f1b8187135
reneenh <reneehamlinteaches@gmail.com>
2022-12-04 18:46:47 -0800

    fixed folders--AGAIN? Also, added LinkedIn and Github logos for footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,14 @@
 import React from "react";
+import GithubLogo from "/assets/images/GithubLogo.png";
+import LinkLogo from "/assets/images/Linked.png";
 
 function Foot() {
   return (
     <footer id="made-by">
       <p>Made with 🌌 by Renée Hamlin</p>
-      <p>icons for social go here</p>
+      {/* icons for social go here */}
+      <img src={GithubLogo} alt="Github" />
+      <img src={LinkLogo} alt="LinkedIn" />
     </footer>
   );
 }
